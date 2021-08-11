60eaf51411d4f69ceba015f3d7ace99c32d594c6
Treveon Edwards <fasuke322@gmail.com>
2021-08-11 18:36:09 -0400

    added weapon to robots

--- dino_vs_robo.py
+++ dino_vs_robo.py
@@ -3,15 +3,16 @@ from herd import Herd
   
   
 fleetx = Fleet()
-    combatnt1 = fleetx.robos[0]
-    combatnt2 = fleetx.robos[1]
-    combatnt3 = fleetx.robos[2] 
+combatnt1 = fleetx.robos[0]
+combatnt2 = fleetx.robos[1]
+combatnt3 = fleetx.robos[2] 
 
 
 herdx = Herd()
-    combatant1 = herdx.dino[0]
-    combatant2 = herdx.dino[1]
-    combatant3 = herdx.dino[2]
+combatant1 = herdx.dino[0]
+combatant2 = herdx.dino[1]
+combatant3 = herdx.dino[2]
+print(combatnt1)
     
 #  def attack_phase(self):
 #      p1.attack minus p2 health
