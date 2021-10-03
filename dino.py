86f152b056c36c4e809f93db9caf25e985c1522e
Treveon <88203620+daisuket330@users.noreply.github.com>
2021-10-03 11:02:07 -0400

    updated dino class and methods

--- dino.py
+++ dino.py
@@ -1,13 +1,29 @@
+from attacks import Attacks
+
+
 class Dinosaur:
-    def __init__(self,name,health,attack):
+    #Constructor
+    def __init__(self, name): #, attack_power):
         self.name = name
-        self.health = health
-        self.attack_power = attack
-        self.alive_status = True    
+        #self.attack_power = attack_power
+        self.health = 100
+        self.attack_types = []
+        self.energy = 100
 
-    def dino_stats(self,name,health,attackpwr):
-        self.name = name
-        self.health = health
-        self.attack_power = attackpwr
+    #Methods
+    def attack(self, robot, index):
+        robot.health -= self.attack_types[index].attack_power
+        self.energy -= 10
 
+    def add_attack(self):
+        crunch = Attacks("Crunch", 60)
+        rend = Attacks("Rend", 45)
+        roar = Attacks("Roar", 30)
+        self.attack_types.append(crunch)
+        self.attack_types.append(rend)
+        self.attack_types.append(roar)
+
+    def get_attack(self, index):
+        print("You selected " + self.attack_types[index].name)
+        return self.attack_types[index]
 
