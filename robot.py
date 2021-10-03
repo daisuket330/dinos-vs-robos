450bd0c8a5e258a818dc99011b4971bdf9f953c4
Treveon <88203620+daisuket330@users.noreply.github.com>
2021-10-03 10:59:43 -0400

    Update robot.py

--- robot.py
+++ robot.py
@@ -13,12 +13,12 @@ class Robot:
     #Methods
     def attack(self, dinosaur):
         dinosaur.health -= self.weapon.attack_power
-        self.energy -= 10
+        self.energy -= 7
         
     def add_weapons(self):
-       first_weapon = Weapon('Ragnarok',9999)
-        second_weapon = Weapon("Plasma machete",5000)
-        third_weapon = Weapon('grenade',8888)
+       first_weapon = Weapon('Ragnarok',99)
+        second_weapon = Weapon("Plasma machete",50)
+        third_weapon = Weapon('grenade',88)
         self.weapon.append(first_weapon)
         self.weapon.append(second_weapon)
         self.weapon.append(third_weapon)
