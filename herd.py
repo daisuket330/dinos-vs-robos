ccd514e1169d984de75abf4ca91033f2c814abfa
Treveon <88203620+daisuket330@users.noreply.github.com>
2021-10-03 11:07:56 -0400

    updated Herd

--- herd.py
+++ herd.py
@@ -1,19 +1,15 @@
-class Dinosaur:
-    def __init__(self,name,health,attack):
-        self.name = name
-        self.health = health
-        self.attack_power = attack
-        
-
-
 class Herd:
     def __init__(self):
-        self.dino = []     
+        self.dino = [] 
+        self.add_dinos()
             
     def add_dinos(self):
-        dino_1 = Dinosaur('Rex',10000,15000)
-        dino_2 = Dinosaur('Queen',8000,10000)
-        dino_3 = Dinosaur('Tony',7000,9500)
+        dino_1 = Dinosaur('Rex',100)
+        dino_2 = Dinosaur('Queen',80)
+        dino_3 = Dinosaur('Tony',50)
         self.dino.append(dino_1)
         self.dino.append(dino_2)
         self.dino.append(dino_3)
+        dino_1.add_attack()
+        dino_2.add_attack()
+        dino_3.add_attack()
